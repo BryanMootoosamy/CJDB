83013848c9f9191da79bce0514192388f087c2c2
BryanMootoosamy <mootoosamy.bryan@gmail.com>
2018-03-28 10:47:27 +0200

    maj

--- footer.php
+++ footer.php
@@ -1,4 +1,19 @@
-<div class="">
+        <footer>
+            <section class="top">
+                <div class="categories">
+                    <?php $arg = array('hide_empty' => FALSE); wp_list_categories($arg); ?>
+                </div>
+                <div class="popularPosts">
 
-</div>
-<?php wp_footer(); ?>
+                </div>
+                <div class="instagram">
+
+                </div>
+            </section>
+            <section class="bottom">
+
+            </section>
+            <?php wp_footer(); ?>
+        </footer>
+    </body>
+</html>
--- header.php
+++ header.php
@@ -22,20 +22,20 @@
     </head>
     <body <?php body_class(); ?>>
         <div id="headerDog"class="headerDog">
-            <div id="socialLink" class="socialLink">
-                <ul>
-                    <li><i class="fab fa-facebook-f"></i></i></li>
-                    <li><i class="fab fa-twitter"></i></li>
-                    <li><i class="fab fa-instagram"></i></li>
-                    <li><i class="fas fa-search"></i></li>
-                </ul>
-            </div>
-            <div class="title">
-                <h1>FooDog</h1>
+            <div class="headerTop">
+                <div id="socialLink" class="socialLink">
+                    <ul>
+                        <li><i class="fab fa-facebook-f"></i></i></li>
+                        <li><i class="fab fa-twitter"></i></li>
+                        <li><i class="fab fa-instagram"></i></li>
+                        <li><i class="fas fa-search"></i></li>
+                    </ul>
+                </div>
+                <div class="title">
+                    <h1>FooDog</h1>
+                </div>
             </div>
             <div class="pageLink">
                 <?php wp_nav_menu() ?>
             </div>
         </div>
-    </body>
-</html>
