19dd31e8ce426939b8077dbc9e14bd9b82594a40
BryanMootoosamy <mootoosamy.bryan@gmail.com>
2018-03-29 09:12:06 +0200

    early burger menu

--- headerBurger.php
+++ headerBurger.php
@@ -3,5 +3,12 @@
         <i class="fas fa-bars"></i>
         <h2>FooDog</h2>
     </div>
+    <div class="center">
+        <p>Digest In Your Inbox </p><form class="emailDigest" method="post">
+            <input type="email" name="email" value=""><button type="submit" name="button">Sign Up</button>
+        </form>
+    </div>
+    <div class="right">
 
+    </div>
 </section>
