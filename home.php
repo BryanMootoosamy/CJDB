a6c35668bf35617aeb3f640ae275895a62819d57
BryanMootoosamy <mootoosamy.bryan@gmail.com>
2018-03-29 15:58:11 +0200

    maj

--- home.php
+++ home.php
@@ -2,11 +2,17 @@
     //Template Name: Home
     get_template_part('head');
     if (!is_singular()) {
-        get_template_part('headerBurger');
+        get_template_part('header');
+    } else {
+        get_template_part("headerBurger");
     }
 ?>
 <main>
-
+    <?php $posts = get_post();
+        foreach ($posts as $post) {
+            echo $post;
+        }
+    ?>
 </main>
 <?php
     get_template_part('footer');
