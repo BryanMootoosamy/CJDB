454ac0d86263a1230445fdc73628343bf7d67aaa
Claude Janssen <claudejanssen.pro@gmail.com>
2018-03-30 01:44:12 +0200

    SearchForm&FileHeader

--- searchform.php
+++ searchform.php
@@ -5,26 +5,18 @@
   * @since FooDog 0.1
   */
 ?>
-
-  <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-
-  <label>
-    <span class="screen-reader-text"><?php echo _x( 'Search for:', 'label', 'foodog' ); ?></span>
-    <input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'foodog' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
-  </label>
-
-  <button type="submit" class="search-submit"><span class="screen-reader-text"><?php echo _x( 'Search', 'submit button', 'foodog' ); ?></span></button>
-
-</form>
-
-<!-- a tester, simpler way
-<fieldset>
-<legend>Recherche</legend>
-<label>
-<input type="text"
-name="s" value="#" id="recherche"
-/>
-</label>
-<input type="submit" id="votre-id" value="Chercher" />
-</fieldset>
-</form> -->
+	<fieldset>
+		<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
+			<label>
+				<p>
+					<input type="hidden" name="post_type[]" value="articles" />
+					<input type="hidden" name="post_type[]" value="post" />
+				</p>
+				<input type="search" class="search-field"
+					placeholder="<?php echo esc_attr_x( 'Search and hit enter…', 'placeholder' ) ?>"
+					value="<?php echo get_search_query() ?>" name="s"
+					/>
+			</label>
+					<i class="fas fa-search"></i>
+		</form>
+	</fieldset>
