fe509f3be753b2cbdd4f7ec873076cb2feaa4f1f
BryanMootoosamy <mootoosamy.bryan@gmail.com>
2018-04-09 16:12:13 +0200

    correct responsive header/footer + reset

--- ucjdb/footer.php
+++ ucjdb/footer.php
@@ -14,19 +14,26 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'ucjdb' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'ucjdb' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'ucjdb' ), 'ucjdb', '<a href="https://inside.becode.org/">Team CJDB</a>' );
-				?>
-		</div><!-- .site-info -->
+		<section class="top">
+                <div class="categories">
+                    <?php $arg = array('hide_empty' => FALSE); wp_list_categories($arg); ?>
+                </div>
+                <div class="popularPosts">
+                    <h3>Popular Posts</h3>
+                    <ul>
+                    	<?php $popular = new WP_Query(array('posts_per_page'=>4, 'meta_key'=>'popular_posts', 'orderby'=>'meta_value_num', 'order'=>'DESC'));
+                    	while ($popular->have_posts()) : $popular->the_post(); ?>
+                    	<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+                    	<?php endwhile; wp_reset_postdata(); ?>
+                    </ul>
+                </div>
+                <div class="instagram">
+                    <p>instagram api</p>
+                </div>
+            </section>
+            <section class="bottom">
+
+</section>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
