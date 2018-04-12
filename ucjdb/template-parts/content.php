6bb201c84a908411d5452ddcee02906f4bf0e10f
Claude Janssen <claudejanssen.pro@gmail.com>
2018-04-12 02:36:12 +0200

    Posted on, not displayed if not on a single page

--- ucjdb/template-parts/content.php
+++ ucjdb/template-parts/content.php
@@ -16,15 +16,12 @@
 			the_title( '<h1 class="entry-title">', '</h1>' );
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+
 		endif;
 
-		if ( 'post' === get_post_type() ) :
-			?>
+		if ( 'post' === get_post_type() && is_single() ) : ?>
 			<div class="entry-meta">
-				<?php
-				ucjdb_posted_on();
-				ucjdb_posted_by();
-				?>
+				<?php ucjdb_posted_by(); ?>
 			</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
