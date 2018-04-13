c20f6b4f052616cf788f145efd660c4e778092e8
Claude Janssen <claudejanssen.pro@gmail.com>
2018-04-13 21:57:59 +0200

    head - meta description & more

--- ucjdb/head.php
+++ ucjdb/head.php
@@ -11,10 +11,15 @@
 
 ?>
 <!doctype html>
-<html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta property="og:locale" content="en_US"/>
+	<meta property="og:type" content="website"/>
+	<meta property="og:title" content="Foodog | Better living with your dog"/>
+	<meta property="og:description" content="Health and wellness advice from vets and nutrition experts, DIY recipes and nutrition education, and a unique editorial perspective on what it means to love and live with your dog."/>
+	<meta property="og:url" content="http://http://turing.claudejanssen.be/wp/"/>
+	<meta property="og:site_name" content="FooDog"/>
 	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
