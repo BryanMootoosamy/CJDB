0044a37d47ee792838287e8dcf51b59e6b0b9652
ocorneillard <dorian.c.collier@gmail.com>
2018-04-16 16:34:51 +0200

    change header

--- ucjdb/header.php
+++ ucjdb/header.php
@@ -1,11 +1,9 @@
-
-
 <body <?php body_class(); ?>>
-	<div id="headerDog"class="headerDog">
+	<div id="headerDog" class="headerDog">
             <div class="headerTop">
                 <div id="socialLink" class="socialLink">
                     <ul>
-                        <li><i class="fab fa-facebook-f"></i></i></li>
+                        <li><i class="fab fa-facebook-f"></i></li>
                         <li><i class="fab fa-twitter"></i></li>
                         <li><i class="fab fa-instagram"></i></li>
                         <li><i class="fas fa-search"></i></li>
